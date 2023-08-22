439637d75641073f113d087d0f133b8396fa7be2
cmistrata <cmistrata@gmail.com>
2023-08-21 21:39:04 -0700

    Use !important properties to avoid mui overrides.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { StyledEngineProvider } from "@mui/material/styles";
 
 export default function Document() {
   return (
@@ -11,12 +10,10 @@ export default function Document() {
           content="Help I've been trapped in this website and can only communicate through the HTML meta element, please before it's too la"
         />
       </Head>
-      <StyledEngineProvider injectFirst>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </StyledEngineProvider>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
     </Html>
   );
 }
