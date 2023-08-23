0e18c9e792001e04d613cdebe90bebbe747081df
cmistrata <cmistrata@gmail.com>
2023-08-23 16:03:53 -0700

    Move analytics component to _app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import "../styles/global.css";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { Analytics } from "@vercel/analytics/react";
 
 export default function Document() {
   return (
@@ -14,7 +13,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
