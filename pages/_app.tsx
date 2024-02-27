c1b9e31fdd38e8ac4e4a519c936e6ae090ed2bfb
cmistrata <cmistrata@gmail.com>
2024-02-26 21:06:10 -0800

    Add smooth scroll and clouds.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,12 @@ import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 import { Analytics } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
+  // Use smooth scroll only after loading the page, so that
+  // if a section is used in the link that section is quickly
+  // jumped to rather than being smooth scrolled to.
+  setTimeout(() => {
+    document.documentElement.style.scrollBehavior = "smooth";
+  }, 1000);
   return (
     <>
       <Component {...pageProps} />
