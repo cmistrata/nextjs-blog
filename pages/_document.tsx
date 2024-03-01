682570a5cc8f456e8b4f7b8c9f0d813ff8c04b01
cmistrata <cmistrata@gmail.com>
2024-02-29 22:29:44 -0800

    Add sitemap.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,10 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Help I've been trapped in this website and can only communicate through the HTML meta element, please before it's too la"
-        />
       </Head>
       <body>
         <Main />
---
+++ pages/sitemap.xml.js
@@ -0,0 +1,49 @@
+import { getAllPostIds } from "../lib/posts";
+
+const URL = "https://www.charliemistrata.com/";
+
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${URL}</loc>
+     </url>
+     <url>
+       <loc>${URL}/posts</loc>
+     </url>
+     ${posts
+       .map((id) => {
+         return `
+       <url>
+           <loc>${`${URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const posts = await getAllPostIds();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
